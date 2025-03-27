d64eb1b8eab67aebcd8892ebe0a9b22977b85970
sara-patino <sara.patino@alumnos.uneatlantico.es>
2025-03-27 16:04:54 +0100

    implementacion de tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className= "text-center text-4xl">Calculadora de Propinas</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
