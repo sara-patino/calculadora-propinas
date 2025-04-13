46affa7e55b3f55589fbb894e16eb755441a6aca
sara-patino <sara.patino@alumnos.uneatlantico.es>
2025-04-13 21:23:49 +0200

    use reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { useReducer } from 'react';
 import MenuItem from './components/MenuItem';
 import OrderContents from './components/OrderContents';
 import TipPercentageForm from './components/TipPercentageForm';
 import OrderTotals from './components/OrderTotals';
 import { menuItems } from './data/db';
-import useOrder from './hooks/useOrder';
+import { orderReducer, initialState } from './reducers/order-reducer';
 
 function App() {
 
-  const { order, addItem, removeItem, tip, setTip, placeOrder } = useOrder()
+  const [state, dispatch] = useReducer(orderReducer, initialState)
 
   return (
     <>
@@ -24,7 +25,7 @@ function App() {
                 <MenuItem
                   key={item.id}
                   item={item}
-                  addItem={addItem}
+                  dispatch={dispatch}
                 />
               ))}
           </div>
@@ -32,22 +33,22 @@ function App() {
         </div>
         
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10" >
-          {order.length > 0 ?  (
+          {state.order.length ?  (
             <>
               <OrderContents
-              order={order}
-              removeItem={removeItem}
+              order={state.order}
+              dispatch={dispatch}
               />
 
               <TipPercentageForm 
-                setTip={setTip}
-                tip={tip}
+                dispatch={dispatch}
+                tip={state.tip}
               />
 
               <OrderTotals
-                order={order}
-                tip={tip}
-                placeOrder={placeOrder}
+                order={state.order}
+                tip={state.tip}
+                dispatch={dispatch}
                 
               />
             </>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,14 +1,15 @@
-import { useMemo } from "react"
+import { useMemo, Dispatch } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
+import { OrderActions } from "../reducers/order-reducer"
 
 type OrderTotalsProps = {
     order: OrderItem[],
     tip: number
-    placeOrder: () => void
+    dispatch: Dispatch<OrderActions>
 }
 
-export default function OrderTotals({order, tip, placeOrder}: OrderTotalsProps) {
+export default function OrderTotals({order, tip, dispatch}: OrderTotalsProps) {
   
     const subtotalAmount = useMemo(() => order.reduce ((total, item) => total + (item.quantity * item.price), 0), 
     [order])
@@ -36,7 +37,7 @@ export default function OrderTotals({order, tip, placeOrder}: OrderTotalsProps) 
         <button
             className="w-full bg-black p-3 uppercase text-white font-bold mt-10"
             disabled={totalAmount === 0}
-            onClick={placeOrder}
+            onClick={() => dispatch({type: 'place-order'})}
             >
         Guardar Orden
             
